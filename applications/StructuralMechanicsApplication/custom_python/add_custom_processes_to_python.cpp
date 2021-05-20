6305934484b5e2f1a9af729643def5d4f6078d94
Sergio <sjimenez@cimne.upc.edu>
2021-05-20 23:05:25 +0200

    Advance in time strategy working

--- applications/StructuralMechanicsApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/StructuralMechanicsApplication/custom_python/add_custom_processes_to_python.cpp
@@ -30,6 +30,8 @@
 #include "custom_processes/impose_rigid_movement_process.h"
 #include "custom_processes/impose_z_strain_process.h"
 #include "custom_processes/distribute_load_on_surface_process.h"
+#include "custom_processes/advance_in_time_high_cycle_fatigue_process.h"
+
 
 namespace Kratos {
 namespace Python {
@@ -96,6 +98,10 @@ void  AddCustomProcessesToPython(pybind11::module& m)
 
     py::class_<DistributeLoadOnSurfaceProcess, DistributeLoadOnSurfaceProcess::Pointer, Process>(m,"DistributeLoadOnSurfaceProcess")
         .def(py::init<ModelPart&, Parameters>());
+
+    py::class_<AdvanceInTimeHighCycleFatigueProcess, AdvanceInTimeHighCycleFatigueProcess::Pointer, Process>(m, "AdvanceInTimeHighCycleFatigueProcess")
+        .def(py::init< ModelPart&, Parameters >())
+        ;
 }
 
 }  // namespace Python.
