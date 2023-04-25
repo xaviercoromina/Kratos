7f68a726b17ead003dc7e17bffd1c546023fbeaa
Vicente Mataix Ferándiz <vmataix@altair.com>
2023-04-25 12:57:07 +0000

    WIP

---
+++ kratos/benchmarking/benchmarking.cpp
@@ -0,0 +1,22 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "benchmarking/benchmarking.h"
+
+namespace Kratos::Benchmarking
+{
+}  // namespace Kratos::Benchmarking.
---
+++ kratos/benchmarking/benchmarking.h
@@ -0,0 +1,23 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+#pragma once
+
+// System includes
+
+// External includes
+
+// Project includes
+
+namespace Kratos::Benchmarking
+{
+}  // namespace Kratos::Benchmarking.
