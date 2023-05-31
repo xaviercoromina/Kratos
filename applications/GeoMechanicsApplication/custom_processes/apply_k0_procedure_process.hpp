b1733dee5814e8001143c27f986940aa1ae81c44
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2023-05-31 16:19:02 +0200

    DegreesToRadians by Anne and cleanup of includes.

--- applications/GeoMechanicsApplication/custom_processes/apply_k0_procedure_process.hpp
+++ applications/GeoMechanicsApplication/custom_processes/apply_k0_procedure_process.hpp
@@ -15,21 +15,16 @@
 // System includes
 #include <cmath>
 #include <iostream>
-#include <string>
 
 // Project includes
 #include "includes/model_part.h"
-#include "utilities/openmp_utils.h"
 #include "utilities/math_utils.h"
 #include "includes/element.h"
 
 // Application includes
-
 #include "includes/kratos_flags.h"
 #include "includes/kratos_parameters.h"
 #include "processes/process.h"
-
-#include "geo_mechanics_application_variables.h"
 
 namespace Kratos
 {
@@ -96,9 +91,7 @@ class ApplyK0ProcedureProcess : public Process
               if (phi < 0. || phi > 90.) {
                   KRATOS_ERROR << "friction angle Phi out of range in ApplyK0ProcedureProcess: " << phi << std::endl;
               }
-              // convert to Radians Use the conversion function of Anne i.s.o. the nonsens to Radians below.
-              phi *= Globals::Pi / 180.;
-              std::fill(k0_vector.begin(), k0_vector.end(), 1.0 - sin(phi));
+              std::fill(k0_vector.begin(), k0_vector.end(), 1.0 - std::sin(MathUtils<>::DegreesToRadians(phi)));
           }
           else if (rProp.Has(K0_VALUE_XX) && rProp.Has(K0_VALUE_YY) && rProp.Has(K0_VALUE_ZZ)) {
               k0_vector[0] = rProp[K0_VALUE_XX];
@@ -106,7 +99,7 @@ class ApplyK0ProcedureProcess : public Process
               k0_vector[2] = rProp[K0_VALUE_ZZ];
           }
           else {
-              KRATOS_ERROR << "Insufficient material dat for K0 procedure process: " << std::endl;
+              KRATOS_ERROR << "Insufficient material data for K0 procedure process: " << std::endl;
           }
           const auto PoissonUR = rProp.Has(POISSON_UNLOADING_RELOADING) ? rProp[POISSON_UNLOADING_RELOADING] : 0.;
 
@@ -140,6 +133,6 @@ class ApplyK0ProcedureProcess : public Process
 
       }
 
-}; //Class
+};
 
-} /* namespace Kratos.*/
+}
